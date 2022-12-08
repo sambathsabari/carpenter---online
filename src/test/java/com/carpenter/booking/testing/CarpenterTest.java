acf8e3d3720f52e9ab473c4746bc43d6c08f9663
S <sambath.s@capgemini.com>
2022-12-08 08:58:51 +0530

    carpenter

--- src/test/java/com/carpenter/booking/testing/CarpenterTest.java
+++ src/test/java/com/carpenter/booking/testing/CarpenterTest.java
@@ -1,4 +1,4 @@
-package com.carpenter.booking.testing;
+/*package com.carpenter.booking.testing;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertNotNull;
@@ -79,5 +79,5 @@ public class CarpenterTest {
 
 
 
-}
+}*/
 
